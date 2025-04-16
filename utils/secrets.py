7105726bdfe24053edefaf5d0cd6cbec6da7297d
hippopotenuse <regisverdin@gmail.com>
2025-04-16 16:57:45 -0700

    Add class for accessing gcp secrets

--- utils/secrets.py
+++ utils/secrets.py
@@ -1,10 +1,10 @@
 from google.cloud import secretmanager
 import json
-import env
+import utils.env as env
 
 DEFAULT_PROJECT_NUM = env.get("GCP_PROJECT_NUM")
 
-def get_secret(secret_id, project_id=DEFAULT_PROJECT_NUM):
+def get(secret_id, project_id=DEFAULT_PROJECT_NUM):
   client = secretmanager.SecretManagerServiceClient()
   name = f'projects/{project_id}/secrets/{secret_id}/versions/latest'
   response = client.access_secret_version(request={"name": name})
--- utils/test_secrets.py
+++ utils/test_secrets.py
@@ -2,5 +2,5 @@ import secrets as s
 
 def test_get_secret():
   secret_id = "test"
-  assert s.get_secret(secret_id)["id"] == "test_id"
-  assert s.get_secret(secret_id)["secret"] == "12345"
+  assert s.get(secret_id)["id"] == "test_id"
+  assert s.get(secret_id)["secret"] == "12345"
