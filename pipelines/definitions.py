ba4ed790b41a26faf1c66ef66aaffac990edf2b5
hippopotenuse <regisverdin@gmail.com>
2025-04-29 12:52:10 -0700

    fix: update import and module reference for hydrovu assets

--- pipelines/definitions.py
+++ pipelines/definitions.py
@@ -1,9 +1,9 @@
 import dagster as dg
-from pipelines.definitions import hydrovu
+from workflows.hydrovu import assets as hydrovu_assets
 
 
 modules = [hydrovu]
-all_assets = dg.load_assets_from_modules([m.assets for m in modules])
+all_assets = dg.load_assets_from_modules([m.assets for m in modules]) #why does this have no module assets? it crashes. ai?
 
 definitions = dg.Definitions(
   assets=all_assets,
