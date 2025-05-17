feaf51fba28a976263045371277b437287b11c60
hippopotenuse <regisverdin@gmail.com>
2025-05-16 20:30:43 -0700

    Fixed oauth so token is cached. Reauthenticate when expired. Clean up fetch code. Write hydrovu locations to FROST. WIP

--- pipelines/utils/auth.py
+++ pipelines/utils/auth.py
@@ -2,23 +2,29 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 import pipelines.utils.secrets as secrets
 
-class AuthStrategy():
-  def __init__(self):
-    pass
-  def apply(self, request):
-    pass
-
-class OAuth2(AuthStrategy):
+class OAuth2():
   def __init__(self, token_url, secret_name):
     self.token_url = token_url
-    secret = secrets.get(secret_name)
-    self.secret = secret
+    self.secret = secrets.get(secret_name)
+    self.client = BackendApplicationClient(client_id=self.secret['id'])
+    self.oauth = OAuth2Session(client=self.client)
+    self.token = self.fetch_new_token()
 
   def __call__(self, request):
-    client = BackendApplicationClient(client_id=self.secret['id'])
-    oauth = OAuth2Session(client=client)
-    token = oauth.fetch_token(token_url=self.token_url, client_id=self.secret['id'],
+    request.headers['Authorization'] = f'Bearer {self.token['access_token']}'
+    return request
+
+  def fetch_new_token(self):
+    return self.oauth.fetch_token(token_url=self.token_url, client_id=self.secret['id'],
             client_secret=self.secret['secret'])
 
-    request.headers['Authorization'] = f'Bearer {token['access_token']}'
-    return request
+
+def is_token_expired(response):
+  if response.status_code == 401:
+    try:
+      error_data = response.json()
+      if error_data.get("error") == "invalid_token":
+        return True
+    except ValueError:
+        pass  # Response is not JSON or does not contain expected fields
+  return False
--- pipelines/workflows/hydrovu/assets.py
+++ pipelines/workflows/hydrovu/assets.py
@@ -1,49 +1,79 @@
 import dagster as dg
 import requests
-from pipelines.utils.auth import OAuth2
 import json
-# import frost_sta_client as fsc
+import frost_sta_client as fsc
+from pipelines.utils.auth import OAuth2, is_token_expired
+
+HYDROVU_PVACD_URL = "https://www.hydrovu.com/public-api/v1/locations/"
+HYDROVU_OAUTH_URL = 'https://hydrovu.com/public-api/oauth/token'
+FROST_URL = "http://localhost:8080/FROST-Server/v1.1"
 
 location_partitions = dg.DynamicPartitionsDefinition(name="hydrovu_locations")
 
+def fetch_paginated(url, handle_page=None):
+  auth = OAuth2(token_url=HYDROVU_OAUTH_URL, secret_name='hydrovu_pvacd')
+  headers={}
+  next_page = 'first page'
+  responses = []
+  while next_page:
+    response = requests.get(url, auth=auth, headers=headers)
+    responses.append(response)
+    if handle_page:
+      handle_page(response)
+
+    if is_token_expired(response):
+      auth.fetch_new_token()
+    next_page = response.headers.get('X-ISI-Next-Page')
+    headers['X-ISI-Start-Page'] = next_page
+  return responses
+
+def convert_location_hydrovu_to_sensorthings(loc):
+  return fsc.Location(
+    name=loc['name'],
+    description="Location of well where measurements are made",
+    properties={
+      'agency': 'PVACD'
+    },
+    location={
+      'type': 'Point',
+      'coordinates': [loc['gps']['latitude'],
+                      loc['gps']['longitude']]
+    },
+    encoding_type="application/geo+json")
+
+
 @dg.asset()
 def hydrovu_locations(context):
-  url = "https://www.hydrovu.com/public-api/v1/locations/list"
-  auth = OAuth2(token_url='https://hydrovu.com/public-api/oauth/token', secret_name='hydrovu_pvacd')
+  url = HYDROVU_PVACD_URL + "list"
+  frost_url = FROST_URL
+  frost_service = fsc.SensorThingsService(frost_url)
 
-  # st_url = "localhost:8080/FROST-Server/v1.1"
-  # frost_service = fsc.SensorThingsService(st_url)
-
-  headers={}
-  next_page = True
-  #TODO: abstract the fetching logic. On each request, check if we need to re-auth with Oauth2.
-  while next_page:
-    res = requests.get(url, auth=auth, headers=headers)
-    for location in res.json():
-      #TODO: this only adds partitions... need to recreate the whole set (in case some are deleted). are there downsides to this?
-      context.instance.add_dynamic_partitions(location_partitions.name, [str(location['id'])])
-      # context.instance.delete_dynamic_partitions(...)
-    ##WRITE LOCATIONS TO sensorthings
-    # st_loc = fsc.Location()
-
-    next_page = res.headers.get('X-ISI-Next-Page')
-    headers['X-ISI-Start-Page'] = next_page
+  for response in fetch_paginated(url):
+    partitions = []
+    for loc in response.json():
+      context.log.info(f"Writing location to FROST: { loc }")
+      # TODO: how to upsert? this creates duplicates when re-run.
+      frost_service.create(convert_location_hydrovu_to_sensorthings(loc))
+      partitions.append(str(loc['id']))
+    # TODO: delete old ones?
+    context.log.info("Creating new partitions.")
+    context.instance.add_dynamic_partitions(location_partitions.name, partitions)
+    # context.instance.delete_dynamic_partitions(...)
 
 @dg.asset(
     deps=[ hydrovu_locations ],
     partitions_def=location_partitions
 )
 def hydrovu_measurements_fetch(context):
-  url = f"https://www.hydrovu.com/public-api/v1/locations/{context.partition_key}/data"
-  auth = OAuth2(token_url='https://hydrovu.com/public-api/oauth/token', secret_name='hydrovu_pvacd')
-  headers={}
-  next_page = True
-  #TODO: abstract fetching logic (same as in hydrovu_locations)
-  while next_page:
-    res = requests.get(url, auth=auth, headers=headers)
-    next_page = res.headers.get('X-ISI-Next-Page')
-    headers['X-ISI-Start-Page'] = next_page
+  url = HYDROVU_PVACD_URL + f"{context.partition_key}/data"
+  def process(response):
+    context.pdb.set_trace()
+  fetch_paginated(url, process)
+  # for i, response in enumerate(fetch_paginated(url)):
+  #   context.log.info(f"Fetching hydrovu measurements page {i} for location {context.partition_key}")
+  #   context.pdb.set_trace()
   
+
 @dg.asset(
     deps=[ hydrovu_measurements_fetch ],
 )
