302d015e7878070c2d6812db39d8a863c837e973
Chase Martin <chasetmartin@gmail.com>
2025-05-22 10:26:45 -0700

    Move gcs defs into defintions file, update get csv script to just use gcs GCSResource

--- pipelines/workflows/cityofroswell/assets.py
+++ pipelines/workflows/cityofroswell/assets.py
@@ -1,46 +1,29 @@
 import dagster as dg
-from dagster_gcp.gcs import GCSPickleIOManager, GCSResource
-
 import frost_sta_client as fsc
 from pipelines.utils.secrets import GCP_PROJECT_NUM
 
 FROST_URL = "http://localhost:8080/FROST-Server/v1.1" #TODO: move to a sensor things util.
 
-
-# get data from GCS bucket for city of roswell
-# gcs_pickle_io_manager = GCSPickleIOManager(
-#   gcs=GCSResource(project=str(GCP_PROJECT_NUM)),
-#   gcs_bucket="roswellbubbler_dev", #TODO: move to env var or config
-#   gcs_prefix="observations"
-# )
-
-gcs = GCSResource(
-    project=str(GCP_PROJECT_NUM),
-    gcs_bucket="roswellbubbler_dev",  # TODO: move to env var or config
-    gcs_prefix="observations"
-)
-
+# Get csv file from GCS bucket
 @dg.asset(
     required_resource_keys={"gcs"},
-    io_manager_key="in_memory",
-    config_schema={
-        "bucket": str,
-        "prefix": str
-    },
 )
 def get_csv_from_gcs(context):
-    # Get csv file from GCS bucket
-    bucket =  context.op_config["bucket"]
-    prefix = context.op_config["prefix"]
-    gcs_client = context.resources.gcs
+    # Get the GCS bucket and prefix from the context
+    gcs = context.resources.gcs
+    client = gcs.get_client()
 
-    blobs = gcs_client.list_blobs(bucket, prefix=prefix)
-    context.log.info(f"Found {len(blobs)} blobs in bucket {bucket} with prefix {prefix}")
-    for blob in blobs:
-        context.log.info(f"Found blob: {blob.name}")
+    # Get the bucket and prefix from the GCS resource
+    bucket = client.bucket(gcs.bucket)
+    prefix = gcs.prefix
+    context.log.info(f"Fetching CSV files from bucket {bucket} with prefix {prefix}")
+
+    blobs = bucket.list_blobs(prefix=gcs.prefix)
+    context.log.info(f"Found {len(list(blobs))} blobs in bucket {gcs.gcs_bucket}")
     
     all_data = []
     for blob in blobs:
+        context.log.info(f"Processing blob: {blob.name}")
         content = blob.download_as_text()
         all_data.append({
             "name": blob.name,
--- pipelines/workflows/cityofroswell/definitions.py
+++ pipelines/workflows/cityofroswell/definitions.py
@@ -1,6 +1,15 @@
 from dagster import Definitions, load_assets_from_modules
 from pipelines.workflows.cityofroswell import assets
+from pipelines.utils.secrets import GCP_PROJECT_NUM
+from dagster_gcp.gcs import GCSResource
 
 defs = Definitions(
-  assets=load_assets_from_modules([assets])
+  assets=load_assets_from_modules([assets]),
+  resources={
+      "gcs": GCSResource(
+          project=str(GCP_PROJECT_NUM),
+          bucket="roswellbubbler_dev",
+          prefix="observations"
+      ),
+  },
 )
