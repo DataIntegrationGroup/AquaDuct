055ddf32c6ff1f5fe118a0fbcf58fc998bbed7bd
Chase Martin <chasetmartin@gmail.com>
2025-05-27 08:47:48 -0700

    Update root level defs

--- pipelines/definitions.py
+++ pipelines/definitions.py
@@ -1,5 +1,17 @@
 from dagster import Definitions
 import pipelines.workflows.hydrovu.definitions as hydrovu_definitions
 import pipelines.workflows.cityofroswell.definitions as cityofroswell_definitions
+from pipelines.utils.resources import SensorThingsResource
 
-defs = Definitions.merge(cityofroswell_definitions.defs)
+shared_resources_def = Definitions(
+    resources={
+        "frost": SensorThingsResource(
+            frost_url="http://localhost:8080/FROST-Server/v1.1"
+        )
+    }
+)
+
+defs = Definitions.merge(
+    cityofroswell_definitions.defs,
+    shared_resources_def,
+    )
